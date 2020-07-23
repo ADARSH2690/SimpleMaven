559c6d24b0294384c203ea4f5e1d119f36306e12
Adarsh <adarsh.kshama@gmail.com>
2020-07-23 17:46:11 +0100

    first commit of maven project

---
+++ src/main/java/pom/codebind2/maven/demo2/App.java
@@ -0,0 +1,13 @@
+package pom.codebind2.maven.demo2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Maven World !" );
+    }
+}
